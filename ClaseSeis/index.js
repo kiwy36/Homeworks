26929dc049d945502f9fa1227621dc196f687f38
kiwy36 <kevinwittner92@gmail.com>
2025-03-20 14:45:26 -0300

    clase 6

---
+++ ClaseSeis/index.js
@@ -0,0 +1,28 @@
+const nuevoElemento = document.getElementById('elemento').textContent = `nuevo elemento`//agrega elementos
+const nuevasComidas = ['pizza','empanadas','milanesas']
+/*
+let resultado = ''; // Variable para almacenar todas las comidas
+
+for (let i = 0; i < nuevasComidas.length; i++) {
+    resultado += `comida nueva: ${nuevasComidas[i]}\n`;
+}
+
+document.getElementById('nuevacomida').textContent = `resultado`;*/
+//cambios usando el dom
+document.querySelector('img').src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2nC2Q13MbGzC-ncnqIqsA4Vs4DVDXGU6TaA&s'
+document.querySelector('img').height=250
+document.querySelector('img').width=250
+document.querySelector('section').style.backgroundColor='red'
+document.querySelector('section').style.fontSize='25px'
+
+
+const newDiv = document.querySelector('body'); // Ahora seleccionamos directamente el elemento
+const newParrafo = document.createElement('p'); // Creamos el párrafo
+const newText = document.createTextNode('ingresando nuevo texto'); // Creamos el nodo de texto
+
+newParrafo.appendChild(newText); // Añadimos el texto al párrafo
+newDiv.appendChild(newParrafo); // Añadimos el párrafo al body
+
+
+
+
