b50008f2ff190e519df5c7485ebf900d41fa7f2a
kiwy36 <kevinwittner92@gmail.com>
2025-03-21 13:37:28 -0300

    tarea parte 6a

--- ClaseSeis/index.js
+++ ClaseSeis/index.js
@@ -15,14 +15,30 @@ document.querySelector('img').width=250
 document.querySelector('section').style.backgroundColor='red'
 document.querySelector('section').style.fontSize='25px'
 
-
+//crear elementos desde el dom
 const newDiv = document.querySelector('body'); // Ahora seleccionamos directamente el elemento
 const newParrafo = document.createElement('p'); // Creamos el párrafo
 const newText = document.createTextNode('ingresando nuevo texto'); // Creamos el nodo de texto
 
 newParrafo.appendChild(newText); // Añadimos el texto al párrafo
 newDiv.appendChild(newParrafo); // Añadimos el párrafo al body
+//////////tarea.js
+const $form = document.querySelector('form')
+$form.addEventListener('submit',function(e){
+    e.preventDefault();
+    return false;
+});
 
+const edadFueraDelEvento = document.querySelector('#edad').value
+console.log(edadFueraDelEvento)
 
+function saludarPersona(){
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const edad = document.querySelector('#edad').value;
+    console.log(`hola ${nombre} ${apellido},de edad ${edad}`)
+}
+const $saludar = document.querySelector('#saludar');
+$saludar.addEventListener('click',saludarPersona)
 
 
